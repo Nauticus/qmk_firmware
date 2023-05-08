80bd65e98d389e63a7b5e48d1c97001795068352
Nauticus <emanuil.d.ganchev@gmail.com>
2023-05-08 19:25:41 +0300

    all good

--- keyboards/moonlander/keymaps/Nauticus/config.h
+++ keyboards/moonlander/keymaps/Nauticus/config.h
@@ -28,4 +28,8 @@
 
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+
 #define IGNORE_MOD_TAP_INTERRUPT
