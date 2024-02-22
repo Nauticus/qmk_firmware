21d8a9aa801cf6c1b0652c6b0298aa72c666abef
Nauticus <emanuil.d.ganchev@gmail.com>
2024-02-22 13:54:30 +0200

    my voyager

---
+++ keyboards/zsa/voyager/keymaps/Nauticus/config.h
@@ -0,0 +1,66 @@
+/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+ * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
+ * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define ORYX_CONFIGURATOR
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+#define ONESHOT_TAP_TOGGLE 1  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 20000  /* Time (in ms) before the one shot key is released */
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define PERMISSIVE_HOLD_PER_KEY
+
+// #undef MOUSEKEY_DELAY
+// #define MOUSEKEY_DELAY          0
+// #undef MOUSEKEY_INTERVAL
+// #define MOUSEKEY_INTERVAL       16
+// #undef MOUSEKEY_WHEEL_INTERVAL
+// #define MOUSEKEY_WHEEL_INTERVAL 90
+// #undef MOUSEKEY_WHEEL_DELAY
+// #define MOUSEKEY_WHEEL_DELAY    0
+// #undef MOUSEKEY_MAX_SPEED
+// #define MOUSEKEY_MAX_SPEED      4
+// #undef MOUSEKEY_TIME_TO_MAX
+// #define MOUSEKEY_TIME_TO_MAX    0
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_MAX_SPEED
+#undef MOUSEKEY_WHEEL_DELAY
+#undef MOUSEKEY_WHEEL_INTERVAL
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_INTERVAL 10
+#define MOUSEKEY_MOVE_DELTA 1
+#define MOUSEKEY_MAX_SPEED 20
+#define MOUSEKEY_TIME_TO_MAX 40
+#define MOUSEKEY_WHEEL_DELAY 10
+#define MOUSEKEY_WHEEL_INTERVAL 80
+#define MOUSEKEY_WHEEL_MAX_SPEED 8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
---
+++ keyboards/zsa/voyager/keymaps/Nauticus/keymap.c
@@ -0,0 +1,225 @@
+// Copyright 2023 ZSA Technology Labs, Inc <@zsa>
+// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// delete word
+#define N_DELW LALT(KC_BSPC)
+
+#define N_CTBS MT(MOD_LCTL, KC_BSPC)
+#define N_ALTQ MT(MOD_LALT, KC_QUOT)
+#define N_ALTZ MT(MOD_LALT, KC_Z)
+
+// change yabai layout
+#define N_CLAY MEH(KC_A)
+#define N_ENSY LT(_SYM, KC_ENT)
+
+// change to second gaming layer
+#define N_TOGT LT(_G_TWO, KC_ENT)
+
+// One-shot mods
+#define N_HYP OSM(MOD_MEH)
+#define N_OGUI OSM(MOD_LGUI)
+#define N_OALT OSM(MOD_LALT)
+#define N_OSFT OSM(MOD_LSFT)
+#define N_OCTL OSM(MOD_LCTL)
+
+#define N_CPPS TD(TD_PCPS)
+
+enum layers {
+    _BASE,  // default layer
+    _G_ONE,    // gaming layer
+    _G_TWO,    // gaming layer
+    _NAV,
+    _SYM,
+    _MOUSE,
+    _FUN,
+};
+
+enum custom_keycodes {
+    OS_NOOP = SAFE_RANGE,
+    OS_N1 = KC_PLUS,
+    OS_N2 = KC_LBRC,
+    OS_N3 = KC_LCBR,
+    OS_N4 = KC_LPRN,
+    OS_N5 = KC_AMPR,
+    OS_N6 = KC_EQL,
+    OS_N7 = KC_RPRN,
+    OS_N8 = KC_RCBR,
+    OS_N9 = KC_RBRC,
+    OS_N0 = KC_ASTR,
+    OS_TEST,
+};
+
+const key_override_t n1_plus_override = ko_make_basic(MOD_MASK_SHIFT, OS_N1, KC_1);
+const key_override_t n2_lbrc_override = ko_make_basic(MOD_MASK_SHIFT, OS_N2, KC_2);
+const key_override_t n3_lcbr_override = ko_make_basic(MOD_MASK_SHIFT, OS_N3, KC_3);
+const key_override_t n4_lprn_override = ko_make_basic(MOD_MASK_SHIFT, OS_N4, KC_4);
+const key_override_t n5_ampr_override = ko_make_basic(MOD_MASK_SHIFT, OS_N5, KC_5);
+const key_override_t n6_eql_override  = ko_make_basic(MOD_MASK_SHIFT, OS_N6, KC_6);
+const key_override_t n7_rprn_override = ko_make_basic(MOD_MASK_SHIFT, OS_N7, KC_7);
+const key_override_t n8_rcbr_override = ko_make_basic(MOD_MASK_SHIFT, OS_N8, KC_8);
+const key_override_t n9_rbrc_override = ko_make_basic(MOD_MASK_SHIFT, OS_N9, KC_9);
+const key_override_t n0_astr_override = ko_make_basic(MOD_MASK_SHIFT, OS_N0, KC_0);
+
+const key_override_t **key_overrides = (const key_override_t *[]) {
+    &n1_plus_override,
+    &n2_lbrc_override,
+    &n3_lcbr_override,
+    &n4_lprn_override,
+    &n5_ampr_override,
+    &n6_eql_override,
+    &n7_rprn_override,
+    &n8_rcbr_override,
+    &n9_rbrc_override,
+    &n0_astr_override,
+    NULL
+};
+
+enum {
+    TD_PCPS = 0,
+};
+
+void pcps_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        register_code(KC_LGUI);
+        register_code(KC_C);
+        unregister_code(KC_C);
+        unregister_code(KC_LGUI);
+    } else if (state->count == 2) {
+        register_code(KC_LGUI);
+        register_code(KC_V);
+        unregister_code(KC_V);
+        unregister_code(KC_LGUI);
+    }
+};
+
+void pcps_reset(tap_dance_state_t *state, void *user_data) {
+    unregister_code(KC_C);
+    unregister_code(KC_V);
+};
+
+// Tap Dance Declarations
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_PCPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, pcps_finished, pcps_reset),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        KC_GRV,    OS_N1,   OS_N2,    OS_N3,   OS_N4,   OS_N5,                    OS_N6,  OS_N7,   OS_N8,   OS_N9,   OS_N0,   KC_BSLS,
+        KC_TAB,    KC_SCLN, KC_COMMA, KC_DOT,  KC_P,    KC_Y,                     KC_F,   KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+        KC_ESC,    KC_A,    KC_O,     KC_E,    KC_U,    KC_I,                     KC_D,   KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+        KC_LSFT,   N_ALTQ,  KC_Q,     KC_J,    KC_K,    KC_X,                     KC_B,   KC_M,    KC_W,    KC_V,    N_ALTZ,  KC_RSFT,
+                                                        N_CTBS, MO(_NAV), N_ENSY, KC_SPC
+    ),
+    // -- Gaming Layers [START] --
+    [_G_ONE] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LALT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     TG(_G_ONE), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                                     KC_SPC,  N_TOGT, XXXXXXX, XXXXXXX
+    ),
+    [_G_TWO] = LAYOUT(
+        KC_ESC,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LALT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LCTL, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+    // -- Gaming Layers [END] --
+    [_NAV] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, N_CPPS,  _______, _______,                   KC_TAB,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,
+        _______, N_OGUI,  N_OALT,  N_OSFT,  N_OCTL,  _______,                   KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  KC_ENT,
+        _______, N_HYP,   _______, _______, _______, _______,                   _______, N_CLAY,  N_DELW,  _______, KC_PGDN, _______,
+                                                     _______, _______, _______, _______
+    ),
+    [_MOUSE] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+        _______, KC_ACL2, _______, KC_WH_U, _______, _______,                      _______, _______, KC_MS_U, _______, _______, _______,
+        _______, KC_ACL1, KC_WH_L, KC_WH_D, KC_WH_R, _______,                      _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
+        _______, KC_ACL0, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+                                                     _______, _______, KC_MS_BTN2, KC_MS_BTN1
+    ),
+    [_SYM] = LAYOUT(
+        _______, C(KC_1), C(KC_2), C(KC_3), C(KC_4), C(KC_5),                   C(KC_6), C(KC_7), C(KC_8), C(KC_9), C(KC_0), KC_DEL,
+        _______, KC_HASH, KC_DLR,  KC_LBRC, KC_RBRC, KC_AMPR,                   _______, _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_PERC, KC_LPRN, KC_RPRN, KC_EQL,                    _______, N_OCTL,  N_OSFT,  N_OALT,  N_OGUI,  _______,
+        _______, KC_AT,   KC_CIRC, KC_LCBR, KC_RCBR, KC_ASTR,                   _______, _______, _______, _______, N_HYP,   _______,
+                                                     _______, _______, _______, _______
+    ),
+    [_FUN] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______, _______, _______, _______, _______, _______,                   _______,    _______, _______, _______, _______, KC_F12,
+        _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,                   TO(_G_ONE), _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_VOLD, KC_VOLU, _______,                   _______,    _______, _______, _______, _______, _______,
+                                                     _______, _______, _______, _______
+    ),
+};
+
+void keyboard_post_init_user(void) {
+    rgblight_disable_noeeprom();
+    rgb_matrix_disable();
+    // debug_enable=true;
+};
+
+// caps word setup
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_MINS:
+        case KC_LSFT:
+        case KC_RSFT:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+};
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case N_CTBS:
+        case N_ENSY:
+            // Immediately select the hold action when another key is tapped.
+            return true;
+        default:
+            // Do not select the hold action when another key is tapped.
+            return false;
+    }
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case N_CTBS:
+            return TAPPING_TERM - 50;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _G_ONE:
+        case _G_TWO:
+        case _FUN:
+            rgb_matrix_enable();
+            // rgb_matrix_sethsv(0, 0, 175);
+            break;
+        default:
+            rgb_matrix_disable();
+    }
+
+    return update_tri_layer_state(state, _NAV, _SYM, _FUN);
+}
